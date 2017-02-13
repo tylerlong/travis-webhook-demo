1618bcc6466df0690844ab9a7a6b60b6b32bfe45
Tyler Long <tyler4long@gmail.com>
2017-02-13 16:28:12 +0800

    Add a test case

---
+++ test/index.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+
+const assert = require('assert')
+
+describe('math', function () {
+  describe('add', function () {
+    it('should get 1 + 1 correct', function () {
+      assert.equal(2, 1 + 1)
+    })
+  })
+})
